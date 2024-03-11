2590b5cf5827def03189c0eb52174181f55cfdeb
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2024-03-11 17:46:14 +0800

    Added glad header and fixing Cmake

--- main.cpp
+++ main.cpp
@@ -1,4 +1,140 @@
+#include <glad/glad.h>
+
+#include <string>
+#include <iostream>
 #include <GLFW/glfw3.h>
+
+static const char* VS = "#version 330 core\n"
+"layout(location = 0) in vec3 aPos;\n"
+"layout(location = 1) in vec3 aColor;\n"
+"\n"
+"out vec3 ourColor;\n"
+"\n"
+"void main()\n"
+"{\n"
+"  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"  ourColor = aColor;\n"
+"}\n"
+"\n";
+
+static const char* FS = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"\n"
+"void main()\n"
+"{\n"
+"  FragColor = vec4(ourColor, 1.0);\n"
+"}\n"
+"\n";
+
+float vertices[] = {
+  // positions         // colors
+   0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+  -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+   0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+};
+
+class Shader
+{
+public:
+  unsigned int ID;
+  // constructor generates the shader on the fly
+  // ------------------------------------------------------------------------
+  Shader(const char* _vertexCode, const char* _fragmentCode)
+  {
+    std::string vertexCode = _vertexCode;
+    std::string fragmentCode = _fragmentCode;
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+    // 2. compile shaders
+    unsigned int vertex, fragment;
+    // vertex shader
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+    checkCompileErrors(vertex, "VERTEX");
+    // fragment Shader
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment, "FRAGMENT");
+    // shader Program
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID);
+    checkCompileErrors(ID, "PROGRAM");
+    // delete the shaders as they're linked into our program now and no longer necessary
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+  }
+  // activate the shader
+  // ------------------------------------------------------------------------
+  void use()
+  {
+    glUseProgram(ID);
+  }
+  // utility uniform functions
+  // ------------------------------------------------------------------------
+  void setBool(const std::string& name, bool value) const
+  {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+  }
+  // ------------------------------------------------------------------------
+  void setInt(const std::string& name, int value) const
+  {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+  }
+  // ------------------------------------------------------------------------
+  void setFloat(const std::string& name, float value) const
+  {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+  }
+
+private:
+  // utility function for checking shader compilation/linking errors.
+  // ------------------------------------------------------------------------
+  void checkCompileErrors(unsigned int shader, std::string type)
+  {
+    int success;
+    char infoLog[1024];
+    if (type != "PROGRAM")
+    {
+      glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+      if (!success)
+      {
+        glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+        std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+      }
+    }
+    else
+    {
+      glGetProgramiv(shader, GL_LINK_STATUS, &success);
+      if (!success)
+      {
+        glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+        std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+      }
+    }
+  }
+};
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  // make sure the viewport matches the new window dimensions; note that width and 
+  // height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
+}
 
 int main(void)
 {
@@ -7,6 +143,14 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
@@ -18,13 +162,52 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+      std::cout << "Failed to initialize GLAD" << std::endl;
+      return -1;
+    }
+
+    // build and compile our shader program
+    // ------------------------------------
+    Shader ourShader(VS, FS); // you can name your shader files however you like
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
         /* Render here */
+        glClearColor(0.2f, 0.4f, 0.4f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // render the triangle
+        ourShader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -32,6 +215,9 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
 
     glfwTerminate();
     return 0;
