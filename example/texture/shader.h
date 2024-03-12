9dfba089c9fabab6f8e746f4913e39328e8e1d6c
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2024-03-13 00:08:35 +0800

    Added camera and texture example projects in

---
+++ example/texture/shader.h
@@ -0,0 +1,98 @@
+#include <glad/glad.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+class Shader
+{
+public:
+  unsigned int ID;
+  Shader(const char* _vertexFile, const char* _fragmentFile)
+  {
+    std::ifstream vStream(_vertexFile);
+    std::ifstream fStream(_fragmentFile);
+    std::stringstream _vertexCode;
+    std::stringstream _fragmentCode;
+    _vertexCode << vStream.rdbuf();
+    _fragmentCode << fStream.rdbuf();
+    std::string vertexCode = _vertexCode.str();
+    std::string fragmentCode = _fragmentCode.str();
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+    // 2. compile shaders
+    unsigned int vertex, fragment;
+    // vertex shader
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+    checkCompileErrors(vertex, "VERTEX");
+    // fragment Shader
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment, "FRAGMENT");
+    // shader Program
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID);
+    checkCompileErrors(ID, "PROGRAM");
+    // delete the shaders as they're linked into our program now and no longer necessary
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+  }
+  void use()
+  {
+    glUseProgram(ID);
+  }
+  void setBool(const std::string& name, bool value) const
+  {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+  }
+  void setInt(const std::string& name, int value) const
+  {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+  }
+  void setFloat(const std::string& name, float value) const
+  {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+  }
+  void setFloat3(const std::string& name, float* value) const
+  {
+    glUniform3f(glGetUniformLocation(ID, name.c_str()), value[0], value[1], value[2]);
+  }
+  void setFloat4(const std::string& name, float* value) const
+  {
+    glUniform4f(glGetUniformLocation(ID, name.c_str()), value[0], value[1], value[2], value[3]);
+  }
+  void setMat4(const std::string& name, bool transpose, float* value) const
+  {
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, transpose, value);
+  }
+
+private:
+  void checkCompileErrors(unsigned int shader, std::string type)
+  {
+    int success;
+    char infoLog[1024];
+    if (type != "PROGRAM")
+    {
+      glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+      if (!success)
+      {
+        glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+        std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+      }
+    }
+    else
+    {
+      glGetProgramiv(shader, GL_LINK_STATUS, &success);
+      if (!success)
+      {
+        glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+        std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+      }
+    }
+  }
+};
